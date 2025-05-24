665c8e4a006825c019406a3e07e8f4405e2957ac
ADMIN <ADMIN@DESKTOP-CSN9LBD>
2025-05-24 22:23:28 +0700

    Kuro

--- HealthFitSystem/Program.cs
+++ HealthFitSystem/Program.cs
@@ -11,9 +11,7 @@ namespace HealthFitSystem
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            //Push on github
-            // Add services to the container.
-            //Healthyfood
+            //Sieu cap vip pro
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));

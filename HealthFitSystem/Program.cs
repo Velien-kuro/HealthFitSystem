fa07d47cdeaae3667c8984fba77f8100e9938cc7
anhocgioi <annhhe187219@fpt.edu.vn>
2025-05-25 16:53:30 +0700

    abcd

--- HealthFitSystem/Program.cs
+++ HealthFitSystem/Program.cs
@@ -12,8 +12,9 @@ namespace HealthFitSystem
             var builder = WebApplication.CreateBuilder(args);
 
             //Sieu cap vip pro
+             // abcxyz
 
-            // aksnaksf
+            
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));

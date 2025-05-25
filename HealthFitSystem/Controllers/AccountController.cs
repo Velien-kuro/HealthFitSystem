6bba50668c2f25981d67114a4ca857d425f22f16
ADMIN <ADMIN@DESKTOP-CSN9LBD>
2025-05-25 20:30:19 +0700

    Kuro

---
+++ HealthFitSystem/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HealthFitSystem.Models;
+using HealthFitSystem.ViewModels;
+
+namespace HealthFitSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<Users> _signInManager;
+        private readonly UserManager<Users> _userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}

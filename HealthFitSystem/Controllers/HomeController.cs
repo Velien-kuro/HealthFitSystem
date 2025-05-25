23c05120c3f14cdddce6ebdf66cadfbb9bb6f13b
anhocgioi <annhhe187219@fpt.edu.vn>
2025-05-25 16:58:43 +0700

    thung3

--- HealthFitSystem/Controllers/HomeController.cs
+++ HealthFitSystem/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HealthFitSystem.Controllers
 {
+
+    // abcxy
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
